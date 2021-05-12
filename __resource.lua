1cd756b194c9f1d321be596b8e6e9b4a37992074
Praryo <cyrrilericv@gmail.com>
2021-05-13 02:40:36 +0800

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,20 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Praryo RP Boombox'
+version '1.0.0'
+
+ui_page 'html/index.html'
+files {
+	'html/*',
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'config.lua',
+	'server/*.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/*.lua',
+}
---
+++ boombox.sql
@@ -0,0 +1,1 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('boombox', 'Boombox', '1', '0', '1');
